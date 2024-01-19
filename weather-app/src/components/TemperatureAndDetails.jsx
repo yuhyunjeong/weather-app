e1afb0b283c53bf081d68fc689b6f8d7e2438ff2
jade <jadejung95@gmail.com>
2024-01-19 21:35:33 +0900

    icons

--- weather-app/src/components/TemperatureAndDetails.jsx
+++ weather-app/src/components/TemperatureAndDetails.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { FaTemperatureThreeQuarters } from "react-icons/fa6";
+import { SlDrop } from "react-icons/sl";
+import { SiWindicss } from "react-icons/si";
 
 const TemperatureAndDetails = () => {
   return (
@@ -13,6 +15,24 @@ const TemperatureAndDetails = () => {
           alt=""
           className="w-20"
         />
+        <p className="text-5xl">5º</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <FaTemperatureThreeQuarters size={18} className="mr-1" />
+            Real fell:
+            <span className="font-medium ml-1">3º</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <SlDrop size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">65%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <SiWindicss size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1">11 km/h</span>
+          </div>
+        </div>
       </div>
     </div>
   );
